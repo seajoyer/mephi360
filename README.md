f09e343dc276ae819b9177c8f6892125db36d9a8
seajoyer <imgarison@gmail.com>
2025-02-06 12:48:52 +0300

    New blank structure created

---
+++ README.md
@@ -0,0 +1,2 @@
+# Student Platform
+A student platform implemented as a Telegram mini app with Python bot, Java Spring backend, and React frontend.
--- app/src/icons/48/addhome.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { Icon } from '@telegram-apps/telegram-ui/dist/types/Icon';
-export declare const Icon48Addhome: ({ ...restProps }: Icon) => JSX.Element;
-//# sourceMappingURL=addhome.d.ts.map
