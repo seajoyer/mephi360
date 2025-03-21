224b55719f78fade7871fcbbd1e76e2a6861c6f4
seajoyer <imgarison@gmail.com>
2025-03-22 00:13:41 +0300

    Refactored pages

---
+++ frontend/src/icons/24/telegram.js
@@ -0,0 +1,24 @@
+import { _ as _extends } from "@swc/helpers/_/_extends";
+import { _ as _object_destructuring_empty } from "@swc/helpers/_/_object_destructuring_empty";
+import { _ as _object_spread } from "@swc/helpers/_/_object_spread";
+import { _ as _object_spread_props } from "@swc/helpers/_/_object_spread_props";
+import { jsx as _jsx } from "react/jsx-runtime";
+
+export const Icon24Telegram = (_param) => {
+  var restProps = _extends({}, _object_destructuring_empty(_param));
+  return /*#__PURE__*/ _jsx("svg", _object_spread_props(_object_spread({
+    width: "24",
+    height: "24",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, restProps), {
+    children: /*#__PURE__*/ _jsx("path", {
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      d: "M21.1244 3.15891C21.1244 3.15891 23.1596 2.37977 22.99 4.27197C22.9334 5.05112 22.4247 7.77812 22.0289 10.7278L20.6722 19.4653C20.6722 19.4653 20.5591 20.7453 19.5415 20.9679C18.5239 21.1905 16.9976 20.1888 16.7149 19.9662C16.4887 19.7992 12.475 17.2948 11.0617 16.0705C10.6659 15.7365 10.2137 15.0687 11.1182 14.2895L17.0541 8.72425C17.7325 8.0564 18.4109 6.49812 15.5842 8.39033L7.66972 13.6774C7.66972 13.6774 6.76521 14.2339 5.06927 13.733L1.39465 12.62C1.39465 12.62 0.0378716 11.7852 2.3557 10.9503C8.00894 8.33463 14.9624 5.66328 21.1244 3.15891Z",
+      fill: "currentColor"
+    })
+  }));
+};
+
+//# sourceMappingURL=telegram.js.map
